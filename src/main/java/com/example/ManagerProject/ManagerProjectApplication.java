1c2c8fabb71ad3a22f476094a6a70b07a2adb6f8
DayanaIA <53835851+DayanaIA@users.noreply.github.com>
2019-08-13 12:39:37 -0500

    u

--- src/main/java/com/example/ManagerProject/ManagerProjectApplication.java
+++ src/main/java/com/example/ManagerProject/ManagerProjectApplication.java
@@ -6,15 +6,15 @@ import com.example.ManagerProject.Object.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 
-
 @SpringBootApplication
-@RestController
+//@RestController
 public class ManagerProjectApplication {
 
 	// private static final String HELLO = "/hello";
